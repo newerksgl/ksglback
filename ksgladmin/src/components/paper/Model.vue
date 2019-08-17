dd30c15b53d1b850eb08b0d8deddbf0b62f04fba
qcha <1258956484@qq.com>
2019-08-17 11:29:48 +0800

    Update Model.vue

--- ksgladmin/src/components/paper/Model.vue
+++ ksgladmin/src/components/paper/Model.vue
@@ -20,7 +20,7 @@
               <el-table-column prop="question" label="名称" width="180"></el-table-column>
               <el-table-column label="操作">
                 <template slot-scope="scope">
-                  <el-button type="text" size="mini" @click="edit(scope.row)">编辑</el-button>
+                  <el-button type="text" size="mini" @click="goedit(scope.row)">编辑</el-button>
                   <el-button size="mini" type="danger" @click="handleDelete(scope.row)">删除</el-button>
                 </template>
               </el-table-column>
@@ -41,7 +41,7 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        <el-button type="primary" @click="edit()">确 定</el-button>
       </div>
     </el-dialog>
 
@@ -53,7 +53,7 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible2 = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible2 = false">确 定</el-button>
+        <el-button type="primary" @click="addModel()">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -64,16 +64,7 @@ export default {
   data() {
     return {
       activeName: "first",
-      tableData: [
-        {
-          qid: "1",
-          question: "选择题"
-        },
-        {
-          qid: "2",
-          question: "填空题"
-        }
-      ],
+      tableData: [],
       form: {
         qid: "",
         question: ""
@@ -85,17 +76,115 @@ export default {
   },
   components: {},
   methods: {
-    edit(row) {
+    handleDelete(row) {
+      if (confirm("确认删除吗?\n此操作不可以恢复！")) {
+        console.log(row.qid);
+        this.request
+          .post("question/del", { id: row.qid })
+          .then(res => {
+            this.$message({
+              showClose: true,
+              message: "删除成功",
+              type: "success",
+              duration: 1000
+            });
+            this.getTableData();
+          })
+          .catch(err => {
+            this.$message({
+              showClose: true,
+              message: "请求失败",
+              type: "error",
+              duration: 1000
+            });
+          });
+      }
+    },
+    goedit(row) {
       this.dialogFormVisible = true;
-      console.log(row.reid);
-      console.log(row.name);
+      console.log(row.qid);
+      console.log(row.question);
       this.form.qid = row.qid;
       this.form.question = row.question;
     },
     goAdd() {
+      this.form.qid = "";
       this.form.question = "";
       this.dialogFormVisible2 = true;
+    },
+    edit() {
+      const question = {
+        qid: this.form.qid,
+        question: this.form.question
+      };
+      this.request
+        .post("question/update", question)
+        .then(res => {
+          this.$message({
+            showClose: true,
+            message: "修改成功",
+            type: "success",
+            duration: 1000
+          });
+          this.getTableData();
+        })
+        .catch(err => {
+          this.$message({
+            showClose: true,
+            message: "请求失败",
+            type: "error",
+            duration: 1000
+          });
+        });
+
+      this.dialogFormVisible = false;
+    },
+    addModel() {
+      const question = {
+        question: this.form.question
+      };
+      this.request
+        .post("question/add", question)
+        .then(res => {
+          this.$message({
+            showClose: true,
+            message: "添加成功",
+            type: "success",
+            duration: 1000
+          });
+          this.getTableData();
+        })
+        .catch(err => {
+          this.$message({
+            showClose: true,
+            message: "请求失败",
+            type: "error",
+            duration: 1000
+          });
+        });
+
+      this.dialogFormVisible2 = false;
+    },
+    getTableData() {
+      this.request
+        .post("question/findAll")
+        .then(res => {
+          this.tableData = res.data;
+          console.log(res.data);
+          //   }
+        })
+        .catch(err => {
+          this.$message({
+            showClose: true,
+            message: "请求失败",
+            type: "error",
+            duration: 1000
+          });
+        });
     }
+  },
+  mounted() {
+    this.getTableData();
   }
 };
 </script>
