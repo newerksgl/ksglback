07a66e3794b40d0a72197b2acc1cdceb92babd89
yunluo-xingchen <yunluo-xingchen@users.noreply.github.com>
2019-08-19 10:07:51 +0800

    谢涛
    完成了试题管理页面

--- ksgladmin/src/components/paper/Question.vue
+++ ksgladmin/src/components/paper/Question.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <el-row>
+      <el-col :span="24">&nbsp;</el-col>
       <el-col :span="15" :offset="6">
         <el-breadcrumb separator-class="el-icon-arrow-right">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
@@ -8,42 +9,204 @@
         </el-breadcrumb>
       </el-col>
       <el-col :span="2" :offset="1">
-        <el-button type="primary" @click="clearShiTi();">添加试题</el-button>
+        <el-button type="primary" @click="clearExas();">添加试题</el-button>
       </el-col>
     </el-row>
+
     <el-row>
-      <el-col :offset="2">
+      <el-col :offset="1">
         <el-tabs v-model="activeName">
-          <el-tab-pane label="试题类表" name="first"></el-tab-pane>
+          <el-tab-pane label="试题列表" name="first">
+            <el-table :data="Exass" style="width: 100%">
+              <el-table-column type="expand">
+                <template slot-scope="props">
+                  <el-form label-position="left" inline class="demo-table-expand">
+                    <el-form-item label="科目">
+                      <span>{{ props.row.sid }}</span>
+                    </el-form-item>
+                    <el-form-item label="题型">
+                      <span>{{ props.row.qid }}</span>
+                    </el-form-item>
+                    <el-form-item label="难度">
+                      <span>{{ props.row.difficulty }}</span>
+                    </el-form-item>
+                    <el-form-item label="题干">
+                      <span>{{ props.row.question_stem }}</span>
+                    </el-form-item>
+                    <el-form-item label="习题解析">
+                      <span>{{ props.row.problem }}</span>
+                    </el-form-item>
+                    <el-form-item label="参考答案">
+                      <span>{{ props.row.reference }}</span>
+                    </el-form-item>
+                    <el-form-item label="录入时间">
+                      <span>{{ props.row.date }}</span>
+                    </el-form-item>
+                  </el-form>
+                </template>
+              </el-table-column>
+              <el-table-column prop="sid" label="科目"></el-table-column>
+              <el-table-column prop="qid" label="题型"></el-table-column>
+              <el-table-column prop="difficulty" label="难度"></el-table-column>
+              <el-table-column prop="date" label="录入时间"></el-table-column>
+              <el-table-column label="操作">
+                <template slot-scope="scope">
+                  <label class="el-icon-edit" @click="edit(scope.row)"></label>
+                  <span>&nbsp;&nbsp;</span>
+                  <label class="el-icon-circle-close" @click="remove(scope.row.eid)"></label>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
         </el-tabs>
       </el-col>
     </el-row>
+
     <el-dialog title="编辑分类" :visible.sync="dialogFormVisible">
-      <el-form :model="form">
-        <el-form-item label="ID" :label-width="formLabelWidth">
-          <el-input v-model="form.sid" autocomplete="off" disabled></el-input>
-        </el-form-item>
-        <el-form-item label="略缩图" :label-width="formLabelWidth">
-          <el-input v-model="form.image" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="名称" :label-width="formLabelWidth">
-          <el-input v-model="form.name" autocomplete="off"></el-input>
-        </el-form-item>
+      <el-form :model="upExas">
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="科目">
+              <el-select v-model="upExas.sid" placeholder="科目">
+                <el-option label="语文" value="1"></el-option>
+                <el-option label="数学" value="2"></el-option>
+                <el-option label="英语" value="3"></el-option>
+                <el-option label="历史" value="4"></el-option>
+                <el-option label="政治" value="5"></el-option>
+                <el-option label="生物" value="6"></el-option>
+                <el-option label="地理" value="7"></el-option>
+                <el-option label="化学" value="8"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="题型">
+              <el-select v-model="upExas.qid" placeholder="选择题型">
+                <el-option label="选择题" value="1"></el-option>
+                <el-option label="填空题" value="2"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="难度">
+              <el-select v-model="upExas.difficulty" placeholder="选择难度">
+                <el-option label="普通" value="1"></el-option>
+                <el-option label="一般" value="2"></el-option>
+                <el-option label="困难" value="3"></el-option>
+                <el-option label="烈狱" value="4"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="题干">
+              <el-input type="textarea" v-model="upExas.question_stem"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="习题解析" prop="problem">
+              <el-input type="textarea" v-model="upExas.problem" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="参考答案" prop="reference">
+              <el-input type="textarea" v-model="upExas.reference" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="注册时间">
+              <el-col :span="8" :offset="1">
+                <el-date-picker
+                  type="date"
+                  placeholder="选择日期"
+                  v-model="upExas.date"
+                  style="width: 100%;"
+                ></el-date-picker>
+              </el-col>
+              <el-col class="line" :span="1" :offset="1">-</el-col>
+              <el-col :span="8" :offset="1">
+                <el-time-picker placeholder="选择时间" v-model="upExas.date" style="width: 100%;"></el-time-picker>
+              </el-col>
+            </el-form-item>
+          </el-col>
+        </el-row>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
       </div>
     </el-dialog>
-    <el-dialog title="添加分类" :visible.sync="dialogFormVisible1">
-      <el-form :model="form">
-        <el-form-item label="名称" :label-width="formLabelWidth">
-          <el-input v-model="form.name" autocomplete="off"></el-input>
-        </el-form-item>
+
+    <el-dialog title="添加试题" :visible.sync="dialogFormVisible1">
+      <el-form :model="Exas">
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="科目" prop="sid">
+              <el-select v-model="Exas.sid" placeholder="科目">
+                <el-option label="语文" value="1"></el-option>
+                <el-option label="数学" value="2"></el-option>
+                <el-option label="英语" value="3"></el-option>
+                <el-option label="历史" value="4"></el-option>
+                <el-option label="政治" value="5"></el-option>
+                <el-option label="生物" value="6"></el-option>
+                <el-option label="地理" value="7"></el-option>
+                <el-option label="化学" value="8"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="题型" prop="qid">
+              <el-select v-model="Exas.qid" placeholder="选择题型">
+                <el-option label="选择题" value="1"></el-option>
+                <el-option label="填空题" value="2"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="难度" prop="difficulty">
+              <el-select v-model="Exas.difficulty" placeholder="选择难度">
+                <el-option label="普通" value="1"></el-option>
+                <el-option label="一般" value="2"></el-option>
+                <el-option label="困难" value="3"></el-option>
+                <el-option label="烈狱" value="4"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="题干">
+              <el-input type="textarea" v-model="Exas.question_stem"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="习题解析" prop="problem">
+              <el-input type="textarea" v-model="Exas.problem" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="参考答案" prop="reference">
+              <el-input type="textarea" v-model="Exas.reference" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="注册时间">
+              <el-col :span="8" :offset="1">
+                <el-date-picker
+                  type="date"
+                  placeholder="选择日期"
+                  v-model="Exas.date"
+                  style="width: 100%;"
+                ></el-date-picker>
+              </el-col>
+              <el-col class="line" :span="1" :offset="1">-</el-col>
+              <el-col :span="8" :offset="1">
+                <el-time-picker placeholder="选择时间" v-model="Exas.date" style="width: 100%;"></el-time-picker>
+              </el-col>
+            </el-form-item>
+          </el-col>
+        </el-row>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible1 = false">取 消</el-button>
-        <el-button type="primary" @click="addFenLei()">确 定</el-button>
+        <el-button type="primary" @click="addFenLei()">提 交</el-button>
       </div>
     </el-dialog>
   </div>
@@ -55,16 +218,60 @@ export default {
   data() {
     return {
       activeName: "first",
-      tableData: [
+      Question: [
+        { qid: "1", question: "选择题", QuestionTest: "普通试题" },
+        { qid: "2", question: "填空题", QuestionTest: "普通试题" }
+      ],
+      Subject: [
+        { sid: "1", name: "语文", image: "" },
+        { sid: "2", name: "数学", image: "" },
+        { sid: "3", name: "英语", image: "" },
+        { sid: "4", name: "历史", image: "" },
+        { sid: "5", name: "政治", image: "" },
+        { sid: "6", name: "生物", image: "" },
+        { sid: "7", name: "地理", image: "" },
+        { sid: "8", name: "化学", image: "" }
+      ],
+      Exas: {
+        sid: "1",
+        qid: "1",
+        question_stem: "",
+        reference: "",
+        problem: "",
+        difficulty: "简单",
+        eid: "",
+        date: "2019-08-17 11:49:11"
+      },
+      upExas: {
+        sid: "",
+        qid: "1",
+        question_stem: "",
+        reference: "",
+        problem: "",
+        difficulty: "简单",
+        eid: "",
+        date: ""
+      },
+      Exass: [
         {
           sid: "1",
-          name: ".NET",
-          image: ""
+          qid: "1",
+          question_stem: "测试1",
+          reference: "",
+          problem: "",
+          difficulty: "难",
+          eid: "1",
+          date: "2019-08-17"
         },
         {
-          sid: "2",
-          name: "Java",
-          image: ""
+          sid: "1",
+          qid: "1",
+          question_stem: "测试2",
+          reference: "",
+          problem: "",
+          difficulty: "难",
+          eid: "2",
+          date: "2019-08-17"
         }
       ],
       dialogImageUrl: "",
@@ -82,8 +289,18 @@ export default {
     };
   },
   methods: {
-    clearShiTi() {
-      this.dialogFormVisible = true;
+    clearExas() {
+      this.Exas = {
+        sid: "1",
+        qid: "1",
+        question_stem: "",
+        reference: "",
+        problem: "",
+        difficulty: "1",
+        eid: "",
+        date: new Date()
+      };
+      this.dialogFormVisible1 = true;
     },
     show(file) {
       console.log(file.url);
@@ -143,22 +360,22 @@ export default {
         });
     },
     edit(row) {
+      this.upExas = {
+        sid: row.sid,
+        qid: row.qid,
+        question_stem: row.question_stem,
+        reference: row.reference,
+        problem: row.problem,
+        difficulty: row.difficulty,
+        eid: row.eid,
+        date: row.date
+      };
       this.dialogFormVisible = true;
-      console.log(row.sid);
-      console.log(row.image);
-      console.log(row.name);
-      this.form.sid = row.sid;
-      this.form.image = row.image;
-      this.form.name = row.name;
+      console.log(row);
     },
-    goAdd() {
-      this.form.sid = "";
-      this.form.image = "";
-      this.form.name = "";
-      this.dialogFormVisible1 = true;
-    },
-    handleDelete(row) {
-      const id = row.sid;
+    remove(eid) {
+      const id = eid;
+      console.log(id);
       this.request
         .post("subject/findAll", { sid: id })
         .then(res => {
@@ -197,4 +414,16 @@ export default {
 </script>
 
 <style>
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 50%;
+}
 </style>
